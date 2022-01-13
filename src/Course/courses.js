983cfc9ea53984735ec57f3d137cd9252584ed46
LipikaBaniya89 <lipikabania9@gmail.com>
2022-01-13 23:09:37 +0700

    useStatus not working

--- src/Course/courses.js
+++ src/Course/courses.js
@@ -1,14 +1,52 @@
-import './courses.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useRef, useEffect } from "react";
+import './courses.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
 
-function courses() {
 
-    <title>Grade Estimator</title>
+const styles = {
+    textCenter: {textAlign: 'center'},
+    textRight: {textAlign: 'right'},
+};
 
-    return {
+function courses({data, setDataItems}) {
+    const [dataRows, setDataRows] = useState();
 
-    }
+    useEffect(() => {
+        const z = data.map ((v, i) => {
+            return (
+                <tr key={i}>
+                    <td className= {styles.textCenter}>
+                    </td>
+                    <td style={styles.textRight}>{v.year}</td>
+                    <td style={styles.textRight}>{v.sem}</td>
+                    <td style={styles.textRight}>{v.name}</td>
+                    <td style={styles.textRight}>{v.subject}</td>
+                    <td style={styles.textRight}>{v.grade}</td>
+                </tr>
+            );
+        });
+        setDataRows(z);
+    },[data]);
+
+    return (
+        <Container>
+            <Row>
+                <Col xs={5} style={{backgroundColor:'#D3D3D3'}}>
+                    <h1>Grade List</h1>
+                </Col>
+            </Row>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Subject</th>
+                    </tr>
+                </thead>
+                <tbody>{dataRows}</tbody>
+            </Table>
+        </Container>
+
+    );
 
 }
 
